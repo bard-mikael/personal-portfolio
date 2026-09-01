1e84e0789725b6129644e5bbb1bbcab653f5685f
bard-mikael <abuladzemikael@gmail.com>
2026-09-01 13:57:12 +0400

    add  Vite React Tailwind scaffold

---
+++ README.md
@@ -0,0 +1,8 @@
+# mikaelidev
+
+Personal portfolio site.
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-svh">
+      <main />
+    </div>
+  )
+}
+
+export default App
